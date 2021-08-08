78fa5992b19ace70f6f57f935878a30457b4ac44
KatrielKoga <katriel.koga.2012@hotmail.com>
2021-08-08 11:32:17 -0300

    separacao back e front e configuracao deploy

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,21 +1,18 @@
-// import path from 'path'
-
 module.exports = {
-  client: 'mysql',
-  connection: {
-    database: 'pshape',
-    user: 'root', 
-    password: 'password',
-    timezone: 'UTC',
-    dateStrings: true
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    // dirctory: path.resolve(__dirname, 'src', 'database', 'migrations')
-    directory: './src/database/migrations'
-  },
-  useNullAsDefault: true
+	client: 'mysql',
+	connection: {
+		database: process.env.DATABASE,
+		user: process.env.USER,
+		password: process.env.PASSWORD,
+		timezone: 'UTC',
+		dateStrings: true,
+	},
+	pool: {
+		min: 2,
+		max: 10,
+	},
+	migrations: {
+		directory: './src/database/migrations',
+	},
+	useNullAsDefault: true,
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,12 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const routes = require('./routes')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./routes');
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use(routes)
+app.use(routes);
 
-app.listen(3333)
-
+app.listen(process.env.PORT || 3333);
