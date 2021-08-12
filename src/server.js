c9ba5d8224df6d8047ab1d38b28090a2308f3c99
KatrielKoga <katriel.koga.2012@hotmail.com>
2021-08-12 19:26:12 -0300

    edit cors config

--- src/server.js
+++ src/server.js
@@ -3,9 +3,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
 
+var corsOptions = {
+	origin: 'https://personalshape.herokuapp.com',
+	optionsSuccessStatus: 200,
+};
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(routes);
 
